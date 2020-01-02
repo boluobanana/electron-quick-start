87cf29ee27e0597b25a0e642c1db22be64702545
wuyutao <wuyutao@bytedance.com>
2020-01-02 22:29:30 +0800

    feat: add test code
    Change-Id: Ie8adbdc39048d097c896b9e494920b84f36d4b59

--- renderer.js
+++ renderer.js
@@ -4,3 +4,17 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const { remote } = require('electron');
+const BrowserWindow = remote.BrowserWindow;
+
+const openThreeJsDocBtn = document.querySelector('.openThreeJsDoc');
+
+openThreeJsDocBtn.addEventListener('click', () => {
+  const win = new BrowserWindow({
+    width: 500, height: 300, title: 'test', webPreferences: {
+      nodeIntegration: true,
+    }
+  });
+  win.loadURL("https://threejs.org/docs/#api/en/geometries/BoxBufferGeometry");
+
+});
